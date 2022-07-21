8f629d78d1f9ba2173527d50ae9bda1dfcaae0f8
Sahiti Gabrani <sahitigabrani@gmail.com>
2022-07-21 10:21:35 +0530

    added devx logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import background_image from "../images/background.png";
-
+import logo_image from "../images/logo.svg";
+import styles from "../App.css"
 function Home() {
 
     return (
@@ -8,10 +9,20 @@ function Home() {
           height: 1117,
           marginTop: -20,
         }}>
-            <p>
-              Home Page!
-            </p>
+
+          <img src={logo_image} style={{
+            width: "462px",
+            height: "136px",
+            left: "143px",
+            top: "541px",
+            position: "absolute",
+            filter: "drop-shadow(0px, 4px, 4px, rgba(0, 0, 0, 0.25))",
+            
+          }}> 
+          </img>
+
         </div>
+        
       );
 }
 export default Home;
