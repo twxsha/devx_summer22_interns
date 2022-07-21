29b32d5ead2617ea6ebda8118f82c7a907a9a1bc
dianaestrada417 <estrada.diana120@gmail.com>
2022-07-20 19:13:22 -0700

    added gif to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
-function Home() {
+import gifImage from "../../src/gif.gif";
+import logoImage from "../../src/logo.png";
+import "../Home.css";
 
+function Home() {
     return (
         <div className="Home">
-            <p>
-              Home Page!
-            </p>
+          <div className="leftLogo">
+            <img src={logoImage} alt="logo loading"/>
+          </div>
+          <div className="rightGif">
+            <img src={gifImage} alt="gif loading" />
+          </div>
         </div>
       );
 }
