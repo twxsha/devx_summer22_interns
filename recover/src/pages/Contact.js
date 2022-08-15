28b0b91f05e3553a40f577686e68569e49fd44c3
ConstantineWang <constantinewang@ucla.edu>
2022-08-15 07:21:51 -0700

    Add contact page, deploy firebase project

--- recover/src/pages/Contact.js
+++ recover/src/pages/Contact.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
 import '../style/Contact.css'
+import '../style/App.css'
+import '../style/Section.css'
 
 class Contact extends Component {
+    submitMessage=()=>{
+
+    }
+
+     
+    componentDidMount(){
+        var inputWrappers = document.getElementsByClassName("inputBox");
+        for(let i=0; i<inputWrappers.length; i++){
+            inputWrappers[i].style.setProperty('--animate-duration', 0.2*i+0.5+'s');
+        }
+        var submitWrapper = document.getElementsByClassName("submitWrapper");
+        submitWrapper[0].style.setProperty('--animate-duration', 1.5+'s');
+        document.getElementById("goBackBtn").addEventListener("click", ()=>{
+            window.location.href="/";
+        })
+
+    }
     render() {
         return (
             <div className='contactWrapper'>
-                hihihi
+                <div className='contactInner'>
+                    <form className='contactForm'>
+                        <h1 className=''>Contact Us</h1>
+                        <div className='inputBox animate__animated animate__backInUp'>                        
+                            <input type="text" required></input>
+                            <span>Name</span>
+                        </div>
+                        <div className='inputBox animate__animated animate__backInUp'>                        
+                            <input type="text" required></input>
+                            <span>Email</span>
+                        </div>
+                        <div className='inputBox animate__animated animate__backInUp'>                        
+                            <textarea required></textarea>
+                            <span>Message</span>
+                        </div>
+                        <div className='submitWrapper animate__animated animate__backInUp ' > 
+                            <div className='submitBtn' id="submitBtn"> 
+                                Submit
+                            </div>
+                            <div className='submitBtn' id="goBackBtn"> 
+                                Go Back
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                
             </div>
         );
     }
--- recover/src/pages/Recruitment.js
+++ recover/src/pages/Recruitment.js
@@ -9,11 +9,11 @@ function Recruitment() {
         <div className="Recruitment" id="Recruitment" >
             <div className="textWrapperLeft" >
               <div data-aos="fade-in" data-aos-duration="4000"></div>
-                <h1 className="title" data-aos="fade-up">Recruitment</h1>
-                <p className="p1" data-aos="fade-up">At DevX, we offer both begginer friendly and experienced roles.
+                <h1 className="title" data-aos="fade-up" >Recruitment</h1>
+                <p className="p1" data-aos="fade-up" data-aos-delay="100">At DevX, we offer both begginer friendly and experienced roles.
 </p>
-                <p className="p1" data-aos="fade-up">While we offer several developer roles, DevX is not only for students interested in technical experience. We also offer roles for Product Managers, Designers and Marketers.</p>
-                <p className="p1" data-aos="fade-up">Our recruitment for Fall of 2022 is coming up soon!</p>
+                <p className="p1" data-aos="fade-up" data-aos-delay="200">While we offer several developer roles, DevX is not only for students interested in technical experience. We also offer roles for Product Managers, Designers and Marketers.</p>
+                <p className="p1" data-aos="fade-up" data-aos-delay="300">Our recruitment for Fall of 2022 is coming up soon!</p>
             </div>
         </div>
         <TransitionBtn navUrl="#Project"/>
