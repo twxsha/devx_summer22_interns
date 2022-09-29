788ab3cb04f07afd43eb9ab17822f58cce933130
twisha <twisha.jai@gmail.com>
2022-09-28 20:22:49 -0700

    updated link to devx gmail

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -11,7 +11,7 @@ class Events extends React.Component {
             <div data-aos="fade-in" data-aos-duration="6000"></div>
             <h1 className="title" data-aos="fade-down" data-aos-delay="0">EVENTS</h1>
             <div className="calendar" id="cal" data-aos="fade-down" data-aos-delay="0">
-              <div className = "responsive" dangerouslySetInnerHTML={{ __html: "<iframe src='https://calendar.google.com/calendar/embed?src=c_kgm6ebnif741k5sgqnhdq879e8%40group.calendar.google.com&ctz=America%2FLos_Angeles' width='1200px' height='800px' style='border: 25px solid #5DABAA;' />"}} />;
+              <div className = "responsive" dangerouslySetInnerHTML={{ __html: "<iframe src='https://calendar.google.com/calendar/embed?src=ucladevx%40gmail.com&ctz=America%2FLos_Angeles' width='1200px' height='800px' style='border: 25px solid #5DABAA;' />"}} />;
             </div>
             <p className="SubtitleText">
                 <a href="https://www.google.com/">RSVP for our next event!</a>
